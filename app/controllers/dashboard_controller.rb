7f05f39c64cc0440ee2a7131812787fa44d69f55
Flipez <code@brauser.io>
2019-06-20 10:03:47 +0200

    Dont make the dashboard crash if data is missing
    Signed-off-by: Flipez <code@brauser.io>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   def show
-    @data = JSON.parse(RedisClient.client.get('repo_status_data'))
+    @data = begin
+      JSON.parse(RedisClient.client.get('repo_status_data').to_s)
+    rescue JSON::ParserError
+      nil
+    end
     @last_sync = Time.at(RedisClient.client.get('repo_status_time').to_i)
   end
 end
