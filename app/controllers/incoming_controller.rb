57204e4536c86bd9f1ee6c5ee71fcd0fb74bb7a1
Flipez <code@brauser.io>
2019-07-16 07:42:47 +0200

    skip login check

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,7 +1,8 @@
 class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :logged_in
 
   def github
-    Raven.capture_message('Received hook') 
+    Raven.capture_message('Received hook')
   end
 end
