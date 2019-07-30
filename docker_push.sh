e0fcddca3ba53072c77e73f6e7ea0c24c9734963
Tim Meusel <tim@bastelfreak.de>
2019-07-30 20:53:03 +0200

    docker: publish image to correct organisation

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker build -t voxpupuli/vox-pupli-tasks:latest .
+docker build -t voxpupuli/vox-pupli-tasks:latest -t "voxpupuli/vox-pupuli-tasks:$(git rev-parse --short HEAD)" .
 docker push voxpupuli/vox-pupuli-tasks:latest
