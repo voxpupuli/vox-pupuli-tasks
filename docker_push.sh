bb5560335f745cecde503c7f3a395b5944c7de87
Tim Meusel <tim@bastelfreak.de>
2019-07-30 20:17:06 +0200

    docker: publish image to correct organisation

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker build .
-docker push "${DOCKER_USERNAME}/vox-pupuli-tasks"
+docker push "voxpupuli/vox-pupuli-tasks"
