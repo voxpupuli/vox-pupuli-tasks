d11df1223a9038368819eb576eb2569df88a15d2
Robert Müller <code@brauser.io>
2019-07-30 20:52:07 +0200

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker build -t voxpupuli/vox-pupli-tasks .
-docker push "voxpupuli/vox-pupuli-tasks"
+docker build -t voxpupuli/vox-pupli-tasks:latest .
+docker push voxpupuli/vox-pupuli-tasks:latest
