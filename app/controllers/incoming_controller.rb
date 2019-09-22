3235acf5bde422d8fd08ba7880d7b99b431b122b
Flipez <code@brauser.io>
2019-09-22 16:24:00 +0200

    remove debug log
    Signed-off-by: Flipez <code@brauser.io>

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -12,10 +12,6 @@ class IncomingController < ApplicationController
 
     verify_signature(payload)
 
-    # For debug purposes we currently log the requets into Sentry
-    # TODO: Remove this line since the GithubEvent handler reports unknown events.
-    Raven.capture_message('Received hook', extra: useable_body)
-
     GithubEvent.new(useable_body, request.headers['X-GitHub-Event'])
   end
 
