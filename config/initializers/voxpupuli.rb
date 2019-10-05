17ef073c4015fcdfd9089c8be815a0581ce51da7
Flipez <code@brauser.io>
2019-10-05 19:23:50 +0200

    please the reviewer
    Signed-off-by: Flipez <code@brauser.io>

--- config/initializers/voxpupuli.rb
+++ config/initializers/voxpupuli.rb
@@ -5,9 +5,6 @@ VOXPUPULI_CONFIG = YAML.load_file('config/voxpupuli.yml')
 LEGACY_OR_BROKEN_NOBODY_KNOWS = VOXPUPULI_CONFIG['legacy_or_broken_nobody_knows']
 # define some versions that we want to match against
 PUPPET_SUPPORT_RANGE = VOXPUPULI_CONFIG['puppet_support_range']
-# Ubuntu 18.04 got released, but Puppet doesn't work yet on it
-# https://tickets.puppetlabs.com/browse/PA-1869
-# https://github.com/camptocamp/facterdb/pull/82#event-1600066178
 
 UBUNTU_SUPPORT_RANGE  = VOXPUPULI_CONFIG.dig('support_ranges', 'ubuntu')
 DEBIAN_SUPPORT_RANGE  = VOXPUPULI_CONFIG.dig('support_ranges', 'debian')
