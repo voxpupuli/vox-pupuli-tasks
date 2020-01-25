9477129ee71f2c3d148a7b6e14577b83e069c4bc
Flipez <code@brauser.io>
2020-01-25 15:52:02 +0100

    please the linter
    Signed-off-by: Flipez <code@brauser.io>

--- config/initializers/rack_monkey_patch.rb
+++ config/initializers/rack_monkey_patch.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Monkey patch `stringify_keys` to make rack 2.1.1 compatible with Sidekiq UI Admin panel.
 # Should be removed when 2.1.2 is released.
 # https://github.com/rack/rack/pull/1428
