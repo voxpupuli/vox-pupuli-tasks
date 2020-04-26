25e2ec2b7adcfd836cd3106ac70c278a23e45199
Flipez <code@brauser.io>
2020-04-26 17:47:34 +0200

    remove elasticsearch
    Signed-off-by: Flipez <code@brauser.io>

--- config/initializers/semantic_logger.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# enable logging to elasticsearch
-if Rails.application.credentials.elasticsearch
-  SemanticLogger.add_appender(
-    appender: :elasticsearch,
-    url: Rails.application.credentials.elasticsearch[:host]
-  )
-end
