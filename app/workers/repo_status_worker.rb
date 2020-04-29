7e69e7e4fdb8cd88f99d67c9fc308975c0c43f13
Flipez <code@brauser.io>
2020-04-30 01:12:34 +0200

    a bit cleaner
    Signed-off-by: Flipez <code@brauser.io>

--- app/workers/repo_status_worker.rb
+++ app/workers/repo_status_worker.rb
@@ -89,22 +89,30 @@ class RepoStatusWorker
 
   def refresh_managed_modules
     data = Github.get_file('voxpupuli/modulesync_config', 'managed_modules.yml')
-    RepoStatusData.modulesync_repos = YAML.safe_load(data)
-  rescue StandardError
-    RepoStatusData.modulesync_repos = []
+    RepoStatusData.modulesync_repos = begin
+                                        YAML.safe_load(data)
+                                      rescue StandardError
+                                        []
+                                      end
   end
 
   def refresh_plumbing_modules
     data = Github.get_file('voxpupuli/plumbing', 'share/modules')
-    RepoStatusData.plumbing_modules = data.split("\n")
-  rescue StandardError
-    RepoStatusData.plumbing_modules = []
+    RepoStatusData.plumbing_modules = begin
+                                        data.split("\n")
+                                      rescue StandardError
+                                        []
+                                      end
   end
 
   def refresh_forge_releases
     PuppetForge.user_agent = 'VoxPupuli/Vox Pupuli Tasks'
-    vp = PuppetForge::User.find('puppet')
-    RepoStatusData.forge_releases = vp.modules.unpaginated.map(&:slug)
+    RepoStatusData.forge_releases = begin
+                                      vp = PuppetForge::User.find('puppet')
+                                      vp.modules.unpaginated.map(&:slug)
+                                    rescue StandardError
+                                      []
+                                    end
   end
 
   # Categorizes repository based on their locally cached information
