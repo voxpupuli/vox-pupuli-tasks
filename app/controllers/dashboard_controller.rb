6050ec7c42b1d16455fc59db873891911e7bdb40
Flipez <code@brauser.io>
2020-05-07 21:39:39 +0200

    fix last status
    Signed-off-by: Flipez <code@brauser.io>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   def show
     @repositories = Repository.all
     @statuses = @repositories.map(&:current_status)
+    @last_status = RepositoryStatus.last
   end
 
   def about; end
