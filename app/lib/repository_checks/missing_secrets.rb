64b3a6f036c537c359a26955d22e48bb9cac792e
Flipez <code@brauser.io>
2021-08-15 13:01:10 +0200

    check .sync if it has the required content
    Signed-off-by: Flipez <code@brauser.io>

--- app/lib/repository_checks/missing_secrets.rb
+++ app/lib/repository_checks/missing_secrets.rb
@@ -2,6 +2,20 @@
 
 class MissingSecrets < RepositoryCheckBase
   def perform
-    submit_result :has_secrets, Github.check_file(repo.full_name, '.sync.yml')
+    secrets_file = Github.get_file(repo.full_name, '.sync.yml')
+
+    if secrets_file
+      content = YAML.safe_load(secrets_file)
+      submit_result :has_secrets, !content.dig('.travis.yml', 'secure').nil?
+    else
+      submit_result :has_secrets, false
+    end
+  rescue StandardError => e
+    submit_result :has_secrets, false
+
+    ##
+    # We reraise the error to get handled by Sentry but rescue it
+    # first to make sure the check has a valid (false) state
+    raise
   end
 end
