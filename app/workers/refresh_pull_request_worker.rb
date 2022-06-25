2dfec6d5f7ce96e44b7c28775cc0e1c9289f1a20
Flipez <code@brauser.io>
2022-06-25 20:56:26 +0200

    Fix undefined local variable or method `id' for RefreshPullRequestWorker
    Signed-off-by: Flipez <code@brauser.io>

--- app/workers/refresh_pull_request_worker.rb
+++ app/workers/refresh_pull_request_worker.rb
@@ -10,7 +10,7 @@ class RefreshPullRequestWorker
       # use tally. the arrays can contain strings and ints. we cannot call sort on heterogenous arrays
       # see also https://twitter.com/BastelsBlog/status/1311421975827546112
       if (job.args.tally.to_a - this_args).empty? && (job.item['class'] == name)
-        Raven.capture_message('Duplicate Job, discarding', extra: { id: id, worker: name })
+        Raven.capture_message('Duplicate Job, discarding', extra: { args: this_args, worker: name })
         return false
       end
     end
